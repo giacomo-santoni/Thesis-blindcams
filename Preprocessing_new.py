2ff8d779e6cab558f1cc1bf35ce41aab494b4755
giacomo-santoni <133137485+giacomo-santoni@users.noreply.github.com>
2023-09-22 10:45:58 +0200

    with other functions

--- Preprocessing_new.py
+++ Preprocessing_new.py
@@ -1,9 +1,11 @@
 import drdf
-import matplotlib.pyplot as plt
 import numpy as np
 import RootPreprocessing_new as rooprep
 from sklearn.preprocessing import RobustScaler
 from sklearn.model_selection import train_test_split
+
+
+#DRDF FILE - SIMULATED DATA
 
 def load_drdf(fname):
   reader = drdf.DRDF()
@@ -19,6 +21,9 @@ def load_drdf(fname):
       events.append((event, hits_map))
   return events
 
+file_drdf_list = ["/Users/giacomosantoni/Desktop/TESI/Progetto_ML/blindcams/data/initial_data_good/response.drdf","/Users/giacomosantoni/Desktop/TESI/Progetto_ML/blindcams/data/new_data/response_pde25.drdf","/Users/giacomosantoni/Desktop/TESI/Progetto_ML/blindcams/data/new_data2/response.drdf"]
+file_root_list = ["/Users/giacomosantoni/Desktop/TESI/Progetto_ML/blindcams/data/initial_data_good/sensors.root","/Users/giacomosantoni/Desktop/TESI/Progetto_ML/blindcams/data/new_data/sensors_pde25.root","/Users/giacomosantoni/Desktop/TESI/Progetto_ML/blindcams/data/new_data2/sensors.root"]
+
 def CamList(fname):
     cam_list = []
     file = load_drdf(fname)
@@ -26,59 +31,57 @@ def CamList(fname):
         cam_list.append(cam)
     return cam_list
 
+#creo una matrice 1000 righe x 54 colonne. ogni elemento è una camera 31x31
 def AllImages(fname): 
-    all_images = []
+    all_images_list = []
     cam_list = CamList(fname)
     file = load_drdf(fname)
     for i in range(len(file)):
         ph_matrix = []
         for cam in cam_list:
             ph_matrix.append(file[i][1][cam])
-        all_images.append(ph_matrix)
+        all_images_list.append(ph_matrix)
+    all_images = np.array(all_images_list)
     return all_images
 
-def FlattenSingleCam(fname):
-    all_photons_single_cam = []
+def DimensionsData(fname):
     all_images = AllImages(fname)
-    for i in range(len(all_images)):
-        photons_single_cam = []
-        for j in range(len(all_images[0])):
-            photons_single_cam.append(all_images[i][j].flatten())
-        all_photons_single_cam.append(photons_single_cam)
-    return all_photons_single_cam
+    cam_list = CamList(fname)
+    nr_events = len(all_images)
+    nr_pixels = len(all_images[0][0])
+    nr_cams = len(cam_list)
+    nr_tot_events = nr_cams*nr_events
+    nr_pixels_1_ev = nr_pixels*nr_pixels*nr_cams
+    return nr_events, nr_pixels, nr_cams, nr_tot_events, nr_pixels_1_ev
+
+#creo una matrice di 1000 righe e 51894 colonne. Così per ogni evento, la riga corrispondente presenta tutti i pixel di tutte le camere
+def PixelsAllCamsPerEvents(fname):
+    all_images = AllImages(fname)
+    nr_events, nr_pixels, nr_cams, nr_tot_events, nr_pixels_1_ev = DimensionsData(fname)
+    pixels_to_scale = []
+    for images_all_cams_per_ev in all_images: 
+        pixels_all_cams_per_ev = images_all_cams_per_ev.flatten()
+        pixels_to_scale.append(pixels_all_cams_per_ev)
+    pixels_to_scale = np.asarray(pixels_to_scale)
+    pixels_to_scale_matrix = pixels_to_scale.reshape(nr_events,nr_pixels_1_ev,1)
+    return pixels_to_scale_matrix
 
 #applico lo scaling su vettori colonne che sono le camere 31x31 appiattite
 def ScalingData(fname):
-    all_photons_single_cam_scaled = []
-    all_images = AllImages(fname)
-    all_photons_single_cam = FlattenSingleCam(fname)
-    for i in range(len(all_images)):
-        ph_matrix_scaled = []
-        for j in range(len(all_images[0])):
-            transformer = RobustScaler().fit(all_photons_single_cam[i][j].reshape(-1,1))
-            scaled_data = transformer.transform(all_photons_single_cam[i][j].reshape(-1,1))
-            ph_matrix_scaled.append(scaled_data)
-        all_photons_single_cam_scaled.append(ph_matrix_scaled)
-    return all_photons_single_cam_scaled
-
-def MatricesList(fname):
-    all_images = AllImages(fname)
-    dim_matrix = len(all_images[0][0])
-    all_photons_single_cam_scaled = ScalingData(fname)
-    #dentro la matrice 1000righex54colonne, prendo un elemento, che è un array di 961 pixels, e lo trasformo in una matrice 31x31
-    all_images_scaled = []
-    index = 0
-    for i in range(len(all_images)):
-        all_images_scaled_per_ev = []
-        for j in range(len(all_images[0])):
-            matrix = np.array(all_photons_single_cam_scaled[i][j][index:index + dim_matrix * dim_matrix]).reshape((dim_matrix, dim_matrix))
-            all_images_scaled_per_ev.append(matrix)
-        all_images_scaled.append(all_images_scaled_per_ev)
+    pixels_to_scale_matrix = PixelsAllCamsPerEvents(fname)
+    nr_events, nr_pixels, nr_cams, nr_tot_events, nr_pixels_1_ev = DimensionsData(fname)
+    photons_scaled_all_ev = []
+    for pixels in pixels_to_scale_matrix:
+        transformer = RobustScaler().fit(pixels)
+        photons_scaled_in_cam = transformer.transform(pixels)
+        photons_scaled_all_ev.append(photons_scaled_in_cam)
+    photons_scaled_all_ev_matrix = np.asarray(photons_scaled_all_ev)
+    all_images_scaled = photons_scaled_all_ev_matrix.reshape(nr_events,nr_cams,nr_pixels,nr_pixels)
     return all_images_scaled
 
 #ora ho una matrice 1000righex54colonne dove un elemento è una matrice 31x31. Per essere consistente con i dati di root la voglio trasformare in un array di 54000 elementi
 def Flattening(fname):
-    all_images_scaled = MatricesList(fname)
+    all_images_scaled = ScalingData(fname)
     all_images_scaled_1d = []
     for sublist in all_images_scaled:
         all_images_scaled_1d.extend(sublist)
@@ -88,8 +91,9 @@ def Reshaping(fname):
     all_images_scaled_1d_reshaped = []
     all_images = AllImages(fname)
     all_images_scaled_1d = Flattening(fname)
+    nr_events, nr_pixels, nr_cams, nr_tot_events, nr_pixels_1_ev = DimensionsData(fname)
     for data in all_images_scaled_1d:
-        data_new = data.reshape(len(all_images[0][0]),len(all_images[0][0]),1)
+        data_new = data.reshape(nr_pixels,nr_pixels,1)
         all_images_scaled_1d_reshaped.append(data_new)
     return all_images_scaled_1d_reshaped
 
@@ -97,9 +101,8 @@ def Preprocessing(fname):
     load_drdf(fname)
     CamList(fname)
     AllImages(fname)
-    FlattenSingleCam(fname)
+    PixelsAllCamsPerEvents(fname)
     ScalingData(fname)
-    MatricesList(fname)
     Flattening(fname)
     return Reshaping(fname)
 
@@ -119,9 +122,9 @@ def SplitDataset(rname,fname):
 
     return train_ds, val_ds, test_ds, train_inner_ph, val_inner_ph, test_inner_ph
 
-def PrepareDataTraining(rname, fname):
+def PrepareDataSingleFile(rname, fname):
     train_ds, val_ds, test_ds, train_inner_ph, val_inner_ph, test_inner_ph = SplitDataset(rname,fname)
-    all_images_scaled_1d = Flattening(fname)
+    nr_events, nr_pixels, nr_cams, nr_tot_events, nr_pixels_1_ev = DimensionsData(fname)
 
     train_labels_bool = train_inner_ph > 5
     train_labels = train_labels_bool.astype(int)
@@ -132,7 +135,39 @@ def PrepareDataTraining(rname, fname):
     test_labels_bool = test_inner_ph > 5
     test_labels = test_labels_bool.astype(int)
 
-    train_ds = train_ds.reshape(int(0.9*0.9*len(all_images_scaled_1d)), 31, 31, 1)
-    val_ds = val_ds.reshape(int(0.1*len(all_images_scaled_1d)), 31, 31, 1)
-    test_ds = test_ds.reshape(int(0.1*0.9*len(all_images_scaled_1d)),31,31,1)
+    train_ds = train_ds.reshape(train_ds.shape[0], nr_pixels,nr_pixels, 1)
+    val_ds = val_ds.reshape(val_ds.shape[0], nr_pixels, nr_pixels, 1)
+    test_ds = test_ds.reshape(test_ds.shape[0],nr_pixels,nr_pixels,1)
     return train_ds, val_ds, test_ds, train_labels, val_labels, test_labels
+
+def WholeDataset(rname: list,fname: list):
+    #dataset1
+    train_ds1, val_ds1, test_ds1, train_labels1, val_labels1, test_labels1 = PrepareDataSingleFile(rname[0],fname[0])
+    #dataset2
+    train_ds2, val_ds2, test_ds2, train_labels2, val_labels2, test_labels2 = PrepareDataSingleFile(rname[1],fname[1])
+    #dataset3
+    train_ds3, val_ds3, test_ds3, train_labels3, val_labels3, test_labels3 = PrepareDataSingleFile(rname[2],fname[2])
+
+    train_ds = np.concatenate((train_ds1, train_ds2, train_ds3), axis=0)
+    val_ds = np.concatenate((val_ds1, val_ds2, val_ds3), axis=0)
+    test_ds = np.concatenate((test_ds1, test_ds2, test_ds3), axis=0)
+
+    train_labels = np.concatenate((train_labels1, train_labels2, train_labels3), axis=0)
+    val_labels = np.concatenate((val_labels1, val_labels2, val_labels3), axis=0)
+    test_labels = np.concatenate((test_labels1, test_labels2, test_labels3), axis=0)
+    return train_ds, val_ds, test_ds,train_labels, val_labels,test_labels
+
+def FindBlindEvents(rname,fname):
+    ev_cam_state = rooprep.RootPreprocessing(rname,fname)
+    data_prep = Preprocessing(fname)
+    n = 0
+    nr_blind_events = []
+    blind_events = []
+    for i in ev_cam_state:
+        if i == 1:
+            nr_blind_events.append(n)
+        n+=1
+    for i in nr_blind_events:
+        if np.max(i)>100:
+            blind_events.append(data_prep[i])
+    return nr_blind_events, blind_events
